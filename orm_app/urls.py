9a17a3ae2c1f3e14f34a98c3b2410f548ff4866b
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-25 21:57:08 +0500

    orm_app updated

--- orm_app/urls.py
+++ orm_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, job_detail_view
 
 urlpatterns = [
     path('', home_view, name='home_view'),
+    path('jobs/<int:pk>/', job_detail_view, name='job_detail')
 ]
