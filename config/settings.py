b77d6093a6f839f16fec844dd9302ef11ac0b957
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-25 21:48:42 +0500

    Job details have been extended

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'students',
+    'orm_app',
+    'main_app',
+    'teachers',
 ]
 
 MIDDLEWARE = [
--- orm_app/views.py
+++ orm_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from orm_app.models import Jobs
 
-# Create your views here.
+
+def home_view(request):
+    all_jobs = Jobs.objects.all()
+
+    context = {
+        'jobs':all_jobs
+    }
+
+    return render(request, 'index.html', context)
