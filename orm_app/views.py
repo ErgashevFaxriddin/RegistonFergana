7f50af4f9d678805f89f7843f15510e047498162
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-25 22:11:20 +0500

    views.py updated

--- orm_app/views.py
+++ orm_app/views.py
@@ -4,19 +4,11 @@ from orm_app.models import Jobs
 
 def home_view(request):
     all_jobs = Jobs.objects.all()
-
-    context = {
-        'jobs':all_jobs
-    }
-
+    context = {'jobs': all_jobs}
     return render(request, 'index.html', context)
 
 
 def job_detail_view(request, pk):
-    job = get_object_or_404(Jobs, pk=pk)
-
-    context = {
-        'job': job
-    }
-
+    job = get_object_or_404(Jobs, job_id=pk)  # ← job_id !
+    context = {'job': job}
     return render(request, 'job_detail.html', context)
