b2fb5dad35f7326de9baa826610589f70c8a250f
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-27 22:56:38 +0500

    html code updated

--- orm_app/urls.py
+++ orm_app/urls.py
@@ -3,7 +3,6 @@ from .views import employees_list, home, apply
 
 urlpatterns = [
     path('', home, name='home'),
-    path('employees/', employees_list, name='employees_list'),
+    path('employees/', employees_list, name='employees'),  # 👈 changed here
     path('apply/', apply, name='apply'),
-
 ]
