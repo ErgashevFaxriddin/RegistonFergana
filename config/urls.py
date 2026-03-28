7aa1e2db2b5bce548284ee2093b08c0018e24f87
faxriddin ergashev <uzcodermelody@gmail.com>
2026-03-28 13:10:27 +0500

    added static files

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('orm_app.urls')),  # orm_app ichidagi urls.py ni ulaymiz
+    path('', include('orm_app.urls')),
+    path()
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
